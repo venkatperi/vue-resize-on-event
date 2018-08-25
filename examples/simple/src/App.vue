b6aac1675f6352331d2f1db3eceb25f839bfb1d9
Venkat Peri <venkatperi@gmail.com>
2018-08-25 14:59:31 -0400

    initial

---
+++ examples/simple/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <textarea v-model="text" class="input2"
+      v-resize-on-input> </textarea>
+  </div>
+</template>
+
+
+<script>
+  import VueResizeOnEvent from 'code/VueResizeOnEvent'
+
+  export default {
+
+    mixins: [VueResizeOnEvent( 'input' )],
+
+    data() {
+      return {
+        text: '',
+      }
+    },
+
+    methods: {
+      double( x ) {
+        return `${x}${x}`
+      },
+      upper( x ) {
+        return x.toUpperCase()
+      },
+    },
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .input2 {
+    width: 600px;
+    resize: none;
+  }
+</style>
+
